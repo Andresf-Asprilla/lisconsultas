7baaeda3d28ca7cd6cc8929b8fb322eeb8906c4a
Andresf-Asprilla <andresf.asprilla1414@gmail.com>
2024-10-09 23:01:51 -0500

    datalles mejorado un poco

--- src/views/DetallesPacientesView.vue
+++ src/views/DetallesPacientesView.vue
@@ -1,72 +1,102 @@
 <template>
-    <br><h3 class="fst-italic text-white"> <b>Nombre Paciente: {{ paciente.name }} {{ paciente.lastname }} <br>Cedula: {{ paciente.doc }}</b></h3>
-    <div class="accordion shadow-lg rounded" id="accordionPanelsStayOpenExample" style="max-width: 1000px; margin: auto; background-color: #0d3b66;">
-        <div v-for="(item, index) in pacientePrueba" :key="index" class="accordion-item border rounded shadow-sm mb-3 bg-light">
-            <h2 class="accordion-header" :id="'panelsStayOpen-heading' + index">
-                <button class="accordion-button" type="button" 
-                        :data-bs-toggle="'collapse'" 
-                        :data-bs-target="'#panelsStayOpen-collapse' + index" 
-                        aria-expanded="true" 
-                        :aria-controls="'panelsStayOpen-collapse' + index">
-                    Registro Numero: {{ index + 1 }}  
-                </button>
-            </h2>
-            <div :id="'panelsStayOpen-collapse' + index" 
-                 class="accordion-collapse collapse show" 
-                 :aria-labelledby="'panelsStayOpen-heading' + index">
-                <div class="accordion-body">
-                    <h4 class="fw-bold">Perfil Lipídico</h4>
-                    <div class="perfil-lipidico">
-                    <table class="table table-bordered">
-                        <thead class="table-light">
-                            <tr>
-                                <th>Prueba</th>
-                                <th>Descripción</th>
-                                <th>Valor</th>
-                                <th>Unidad</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><strong>Colesterol Total (CHOLT)</strong></td>
-                                <td>Mide la cantidad total de colesterol en la sangre.</td>
-                                <td>{{item.CHOLT}}</td>
-                                <td>mg/dL</td>
-                            </tr>
-                            <tr>
-                                <td><strong>Lipoproteínas de Alta Densidad (HDL)</strong></td>
-                                <td>Conocido como el colesterol bueno, ayuda a eliminar el colesterol de las arterias.</td>
-                                <td>{{item.HDL}}</td>
-                                <td>mg/dL</td>
-                            </tr>
-                            <tr>
-                                <td><strong>Lipoproteínas de Baja Densidad (LDL)</strong></td>
-                                <td>Conocido como el colesterol malo, puede acumularse en las arterias.</td>
-                                <td>{{item.LDL}}</td>
-                                <td>mg/dL</td>
-                            </tr>
-                            <tr>
-                                <td><strong>Triglicéridos (TRIG)</strong></td>
-                                <td>Representa un tipo de grasa en la sangre.</td>
-                                <td>{{item.TRIG}}</td>
-                                <td>mg/dL</td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td><span><router-link :to="{name:'ActualizarPruebasPacientes', params:{id:paciente.id}}" class="btn btn-success">Editar</router-link></span>
-                                </td>
-                                <td><span><button type="button" v-on:click="borrarPaciente(item.id)" class="btn btn-danger">Borrar</button></span></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                </div>
+   <div style="background-color: #0a3a5a; padding: 20px;">
+    <table class="table table-borderless rounded-pill" style="background-color: #ffff; max-width: 1000px; margin: auto; padding: 20px; color: black; border-color: brown; box-shadow: 0 0px 7px rgba(0, 0, 0, 0.3);">
+        <thead>
+            <tr>
+                <th colspan="6" class="text-center rounded-pill" style="background-color: #6aa9e9;">INFORMACION DEL PACIENTE</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td style="font-weight: bold;">Nombre Paciente</td>
+                <td>{{ paciente.name }} {{ paciente.lastname }}</td>
+                <td style="font-weight: bold;">Documento</td>
+                <td>{{ paciente.doc }}</td>
+               
+                <td  style="font-weight: bold;">EPS</td>
+                <td >{{ paciente.eps }}</td>
+            </tr>
+            <tr>
+                <td style="font-weight: bold;">Género</td>
+                <td>{{ paciente.gender }}</td>
+                <td style="font-weight: bold;">Edad</td>
+                <td>{{ paciente.age }}</td>
+            </tr>
+        </tbody>
+    </table>
+
+
+
+      
+ 
+      <div class="accordion accordion-flush" id="accordionFlushExample" style="max-width: 1000px; margin: auto; padding-top: 20px;">
+        <div v-for="(item, index) in pacientePrueba" :key="index" class="accordion-item border border-5 rounded shadow-sm mb-3 bg-white">
+          <h2 class="accordion-header" :id="'panelsStayOpen-heading' + index">
+            <button class="accordion-button" type="button" style="background-color: #6aa9e9; color: white;" 
+                    :data-bs-toggle="'collapse'" 
+                    :data-bs-target="'#panelsStayOpen-collapse' + index" 
+                    aria-expanded="true" 
+                    :aria-controls="'panelsStayOpen-collapse' + index">
+              <h5 style="color: black;"><b>Registro Número: {{ index + 1 }}</b></h5>
+            </button>
+          </h2>
+          <div :id="'panelsStayOpen-collapse' + index" class="accordion-collapse collapse show" :aria-labelledby="'panelsStayOpen-heading' + index">
+            <div class="accordion-body">
+              <h4 class="fw-bold">Perfil Lipídico</h4>
+              <div class="perfil-lipidico">
+                <table class="table table-bordered">
+                  <thead class="table-light">
+                    <tr>
+                      <th>Prueba</th>
+                      <th>Descripción</th>
+                      <th>Valor</th>
+                      <th>Unidad</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td><strong>Colesterol Total (CHOLT)</strong></td>
+                      <td>Mide la cantidad total de colesterol en la sangre.</td>
+                      <td>{{ item.CHOLT }}</td>
+                      <td>mg/dL</td>
+                    </tr>
+                    <tr>
+                      <td><strong>Lipoproteínas de Alta Densidad (HDL)</strong></td>
+                      <td>Conocido como el colesterol bueno, ayuda a eliminar el colesterol de las arterias.</td>
+                      <td>{{ item.HDL }}</td>
+                      <td>mg/dL</td>
+                    </tr>
+                    <tr>
+                      <td><strong>Lipoproteínas de Baja Densidad (LDL)</strong></td>
+                      <td>Conocido como el colesterol malo, puede acumularse en las arterias.</td>
+                      <td>{{ item.LDL }}</td>
+                      <td>mg/dL</td>
+                    </tr>
+                    <tr>
+                      <td><strong>Triglicéridos (TRIG)</strong></td>
+                      <td>Representa un tipo de grasa en la sangre.</td>
+                      <td>{{ item.TRIG }}</td>
+                      <td>mg/dL</td>
+                    </tr>
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td>
+                        <span><router-link :to="{name:'ActualizarPruebasPacientes', params:{id:paciente.id}}" class="btn btn-success">Editar</router-link></span>
+                      </td>
+                      <td>
+                        <span><button type="button" v-on:click="borrarPaciente(item.id)" class="btn btn-danger">Borrar</button></span>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
+          </div>
         </div><br><br>
+      </div>
     </div>
   </template>
-  
 
 <style>
 .perfil-lipidico table {
--- src/views/ListarPacientesView.vue
+++ src/views/ListarPacientesView.vue
@@ -5,10 +5,10 @@
     <div class="card text-center border border-dark" style="background-color: #fff; margin: 50px; padding: 50px;">
     <div class="card-header border border-danger" style="background:#6aa9e9;">
         
-        <h1 class="fw-bolder" style="color: #fff;"> Lista de Pacientes </h1>
+        <h1 class="fw-bolder" style="color: black;"> Lista de Pacientes </h1>
         <form class="d-flex" @submit.prevent="consultarPacientesDoc">
             <input class="form-control me-2" type="search" placeholder="Ingrese el documento del paciente a buscar" aria-label="Search" v-model="searchQuery">
-            <button class="btn btn-outline-success" type="submit">Search</button>
+            <button class="btn btn-outline-success" type="submit">Buscar</button>
         </form>
     </div>
     <div class="card-body">
